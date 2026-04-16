fa21922b0eb0a40095250b1132d147f71268385b
Vikri Usman Rizky <vikri_usman_rizky@ajari.live>
2026-04-16 08:38:09 +0700

    feat: configure GCS backend for Terraform state storage

--- provider.tf
+++ provider.tf
@@ -1,3 +1,10 @@
+terraform {
+  backend "gcs" {
+    bucket  = "terraform-test" # Ganti dengan nama bucket GCS Anda
+    prefix  = "terraform/state"
+  }
+}
+
 provider "google" {
   project = var.project_id
   region  = "asia-southeast2"
